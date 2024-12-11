7a95b37ebd6c960395a5e5f8a60ce3040a00aa84
SamStruthers <sam.struthers2000@gmail.com>
2024-12-11 11:42:26 -0700

    Rerunning script and distance finder to update data for new sites (PFAL, PENN, LBEA, PMAN)

--- scripts/distance_finder.R
+++ scripts/distance_finder.R
@@ -3,14 +3,16 @@ library(sf)
 library(nhdplusTools)
 library(mapview)
 
+`%nin%` = Negate(`%in%`)
 # Poudre HUC
-huc8 <- get_huc8(id="10190007")
+huc8 <- get_huc(id="10190007", type = "huc08")
 
 # Pull all NHDPlusV2 flowines within Poudre Watershed
 flowlines <- get_nhdplus(AOI = huc8, realization = "flowline", t_srs = 4326)
 
 # Pull in site coordinates
-xy <- read_csv('data/cpf_sites.csv') %>%
+xy <- read_csv('data/metadata/location_metadata.csv') %>%
+  filter(Campaign %nin% c("Mainstem - Lower", "Lower Tributary"))%>%
   st_as_sf(coords=c('Long','Lat'), crs=4326) 
 
 # Find NHD comid for each coordinate. NOTE: resolution not great for some sites (see Comanche/Hourglass
@@ -21,7 +23,7 @@ for(i in 1:nrow(xy)){
 }
 
 # Isolate location you want as furthest downstream in the trace
-lowest <- filter(xy, Site_Code=="PBD")
+lowest <- filter(xy, site_code=="PBD")
 
 # Identify all flowlines upstream of that furthest downstream location
 ws_list <- get_UT(flowlines, lowest$comid)
@@ -35,12 +37,12 @@ flowlines <- get_nhdplus(comid=ws_list) %>%
 paths <- get_pathlength(flowlines) %>%
   rename(comid=ID) %>%
   right_join(xy, by='comid') %>%
-  select(Site_Code,comid,distance_upstream_km=pathlength)
+  select(site_code,comid,distance_upstream_km=pathlength)
 
-write.csv(paths, "data/Distance_from_PBD.csv")
+write.csv(paths, "data/metadata/distance_from_pbd.csv")
 
-res_comid <-filter(xy, Type == "Reservoir")
-write.csv(res_comid, "data/res_comid.csv")
+# res_comid <-filter(xy, Type == "Reservoir")
+# write.csv(res_comid, "data/res_comid.csv")
 
 
 mapview(flowlines) + mapview(xy)
